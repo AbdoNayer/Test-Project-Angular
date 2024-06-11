99d046c9115e7cb4018c15b48567dd70e8659c71
anayer <anayer@getgroup.com>
2024-06-12 02:10:03 +0300

    remove-docs

--- docs/browser/chunk-FSPY4QUS.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-RFT32RYC.js";import"./chunk-LPKR6342.js";import"./chunk-FVLWHPLJ.js";import"./chunk-77Q6SZPH.js";import"./chunk-FBA4FM7M.js";import"./chunk-OS6QCVUW.js";export{a as LayoutModule};
--- docs/browser/chunk-K2P7NAU2.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-3JIFKSCC.js";import"./chunk-FBA4FM7M.js";import"./chunk-OS6QCVUW.js";export{b as NotFoundPageModule,a as routes};
